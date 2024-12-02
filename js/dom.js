5444e7e18da457c28ae9b266541bda3bd171aebc
atmabdullaham <atmabdullaham@gmail.com>
2024-12-02 11:09:17 +0600

    practice the getElementByTagName() which return sometihing as a function and we may store this thing in a variable

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,8 @@
-console.log(document.body);
+// console.log(document.getElementsByTagName("h1"
+// ));
+
+const liCollection = document.getElementsByTagName("li");
+console.log(liCollection)
+for (let i = 0; i < liCollection.length; i++) {
+ console.log(liCollection[i])
+}
